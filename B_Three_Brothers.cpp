87c1d069c2f11946daaf78ba28c4658ff40733ed
Muhammad Ammar Qaisar <ammarqaisar1130@gmail.com>
2024-12-03 00:54:04 +0500

    Accepted Solution

---
+++ B_Three_Brothers.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+
+    int res = 6 - a - b;
+    cout<<res<<endl;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int TC = 1;
+    // cin >> TC;
+    // cin.ignore();
+    while (TC--) solve();
+}
